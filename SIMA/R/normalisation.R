d7a56d098527d7f090e0f80a8364cf9d3ac9961a
Vincent Guillemot <vincentguillemot@hotmail.com>
2014-02-18 18:47:55 +0100

    Modification de la fonction de normalisation pour les puces affy et la normalisation FARMS.

--- SIMA/R/normalisation.R
+++ SIMA/R/normalisation.R
@@ -1,8 +1,18 @@
-normalisation <-
-function(noms_fichiers){
-  GCRMA <- justGCRMA(filenames=noms_fichiers, optimize.by="memory")
-  logGCRMA <- exprs(GCRMA)
-  foo <- function(chaine) substr(chaine,1,nchar(chaine)-4)
-  colnames(logGCRMA) <- sapply(colnames(logGCRMA),foo)
-  logGCRMA
+#' Normalization of Affymetrix datasets.
+#' @param inputFolder A character string: where to find the CEL files and the phenotype data.
+#' @return \item{eset}{An object of classe Expression-set.}
+#' @references FARMS.
+#' @title Normalization of Affymetrix
+#' @export affy_normalization
+
+affy_normalization <- function(inputFolder){
+  require(farms)
+  affybatch <- ReadAffy(celfile.path = inputFolder, phenoData=new("AnnotatedDataFrame")) ; gc()
+  eset <- expFarms(affybatch, bgcorrect.method = "none", pmcorrect.method = "pmonly", 
+                   normalize.method = "constant", weight=0.5) ; gc()
+  return(eset)
 }
+
+
+
+
