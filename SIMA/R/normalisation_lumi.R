774f2850648e5ee2011df9d2cda827aec71bb4e8
guegan <j.guegan-ihu@icm-institute.org>
2014-02-20 14:26:24 +0100

    Add average BeadStudio normalization

--- SIMA/R/normalisation_lumi.R
+++ SIMA/R/normalisation_lumi.R
@@ -11,30 +11,24 @@ lumi_normalization <- function(dataFile, phenoFile, bg.method="none", norm.metho
   require(lumi)
   
   # 1. Test on arguments 
-  if (missing (dataFile)){
-    stop("\n\tL'argument dataFile est manquant.\n",call.=FALSE)
-  }
+  if (missing (dataFile))
+    stop("\n\tL'argument dataFile est manquant.",call.=FALSE)
   
-  if(!file.exists(dataFile)){
-    stop(paste0("\n\tLe fichier ",dataFile," n'existe pas.\n"),call.=FALSE)
-  }
+  if(!file.exists(dataFile))
+    stop(paste0("\n\tLe fichier ",dataFile," n'existe pas."),call.=FALSE)
   
-  if (missing (phenoFile)){
-    stop("\n\tL'argument phenoFile est manquant.\n",call.=FALSE)
-  }
+  if (missing (phenoFile))
+    stop("\n\tL'argument phenoFile est manquant.",call.=FALSE)
+    
+  if(!file.exists(phenoFile))
+    stop("\n\tLe fichier ",phenoFile," n'existe pas.",call.=FALSE)
   
-  if(!file.exists(phenoFile)){
-    stop(paste0("\n\tLe fichier ",phenoFile," n'existe pas.\n"),call.=FALSE)
-  }
+  if(all(bg.method != c('none', 'bgAdjust', 'forcePositive', 'bgAdjust.affy')))
+    stop("\n\tLa méthode de correction du bruit de fond ",bg.method," n'est pas supportée !",call.=FALSE)
   
-  if(all(bg.method != c('none', 'bgAdjust', 'forcePositive', 'bgAdjust.affy'))){
-    stop(paste0("\n\tLa méthode de correction du bruit de fond ",bg.method," n'est pas supportée !\n"),call.=FALSE)
-  }
-  
-  if(all(norm.method != c("quantile", "rsn", "ssn", "loess", "vsn", "rankinvariant","average"))){
-    stop(paste0("\n\tLa méthode de normalisation ",norm.method," n'est pas supportée !\n"),call.=FALSE)
-  }
-  
+  if(all(norm.method != c("quantile", "rsn", "ssn", "loess", "vsn", "rankinvariant","average")))
+    stop("\n\tLa méthode de normalisation ",norm.method," n'est pas supportée !",call.=FALSE)
+    
   #2. Read phenotype data file
   pheno = read.table(phenoFile, sep="\t", header=T)
   
@@ -42,22 +36,53 @@ lumi_normalization <- function(dataFile, phenoFile, bg.method="none", norm.metho
   data = tryCatch(
     lumiR.batch(fileList=dataFile,sampleInfoFile=pheno),
     warning=function(w){
-      stop(paste0("\n\tLes IDs du fichier ",phenoFile," ne correspondent pas à ceux de ",dataFile,".\nVérifier votre fichier de phenotype ! \n"),call.=FALSE)
+      stop("\n\tLes IDs du fichier ",phenoFile," ne correspondent pas à ceux de ",dataFile,".\nVérifier votre fichier de phenotype !",call.=FALSE)
     },
     error=function(e) {
-      stop(paste0("\n\tLe fichier ",dataFile," n'est pas un fichier BeadStudio.\n Vérifiez votre fichier !\n"),call.=FALSE)
+      stop("\n\tLe fichier ",dataFile," n'est pas un fichier BeadStudio.\n Vérifiez votre fichier !",call.=FALSE)
     }
   )  
-  
-  # 4. Apply normalization with wrapper function "lumi::lumiExpresso"
-  data.norm = lumiExpresso(data,bgcorrect.param=list(method=bg.method),normalize.param=list(method=norm.method),varianceStabilize.param=list(method="log2"))
-  
-  #Normalisation "average" à implémenter
-  #if(norm.method=="average"){
-  #  
-  #}
-  
-  eSet = batch2eSet(data.norm)
+  cat("\n")
+  # 4. Normalisation
+  if(norm.method=="average"){
+    
+        eSet = lumi_normalization_average(data)
+        
+  }else{
+    # 4. Apply normalization with wrapper function "lumi::lumiExpresso"
+    data.norm = lumiExpresso(data,bgcorrect.param=list(method=bg.method),normalize.param=list(method=norm.method),varianceStabilize.param=list(method="log2"))
+    eSet = batch2eSet(data.norm)
+    
+  }
   
   return(eSet)
+}
+
+lumi_normalization_average <- function(rawLumiBatch){
+  cat("Background Correction: none\n")
+  cat("Variance Stabilizing Transform method: log2\n")
+  cat("Normalisation method: average\n\n")
+  cat("Choose reference array(s) in the table\n")
+  #Modifier la ou les arrays de reference (1=reference, 0=autre array)
+  M = cbind(rawLumiBatch@phenoData@data,refArray=c(1,rep(0, ncol(data)-1)))
+  M=fix(M)
+  cat(paste0("Number of reference arrays = ",length(which(M$refArray=="1")),"\n\n"))
+  cat("Perform average normalization ...\n")
+  dataraw.mat = exprs(rawLumiBatch)
+  datanorm.mat= dataraw.mat
+  ref.mean.array = mean(dataraw.mat[,M$refArray==1])
+  scaling.factor=list()
+  for(i in 1:ncol(dataraw.mat)){
+    scaling.factor[[i]] = ref.mean.array/mean(dataraw.mat[,i])
+    datanorm.mat[,i]=log2(dataraw.mat[,i]*scaling.factor[[i]])
+  }
+  eSet = new('ExpressionSet',
+             exprs = datanorm.mat, 
+             phenoData = new("AnnotatedDataFrame", data@phenoData@data),
+             featureData = new("AnnotatedDataFrame", data@featureData@data),
+             annotation = data@annotation
+  )
+  cat("done.\n")
+  return(eSet)
+  
 }
