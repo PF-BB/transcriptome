ce8ced73cab8152083d1e8711699d88902522e59
guegan <j.guegan-ihu@icm-institute.org>
2014-02-20 11:09:19 +0100

    Add zzz utils

---
+++ SIMA/R/zzz.R
@@ -0,0 +1,30 @@
+#' Internal utils functions.
+#' @title Internal utils functions
+
+
+eSet2lumiBatch <- function(eSet) {
+  lumiBatch = new('LumiBatch', 
+                  exprs = eSet@assayData$exprs, 
+                  phenoData = new("AnnotatedDataFrame", eSet@phenoData@data), 
+                  featureData = new("AnnotatedDataFrame", eSet@featureData@data),
+                  annotation = eSeth@annotation
+  )
+  return(lumiBatch)
+}
+eSet2affyBatch <- function(eSet){
+  affyBatch = new('AffyBatch', 
+                  exprs = eSet@assayData$exprs, 
+                  phenoData = new("AnnotatedDataFrame", eSet@phenoData@data), 
+                  featureData = new("AnnotatedDataFrame", eSet@featureData@data),
+                  annotation = eSeth@annotation
+}
+
+batch2eSet <- function(batch){
+  eSet = new("ExpressionSet", 
+             exprs = batch@assayData$exprs, 
+             phenoData = new("AnnotatedDataFrame", batch@phenoData@data), 
+             featureData = new("AnnotatedDataFrame", batch@featureData@data),
+             annotation = batch@annotation
+  )
+  return(eSet)
+}
