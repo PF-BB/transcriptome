97879f3fe1d53b0ff38ad7b675155510846f9dc2
guegan <j.guegan-ihu@icm-institute.org>
2014-02-27 16:01:08 +0100

    correct bugs in shiny

--- shiny/ui.R
+++ shiny/ui.R
@@ -2,15 +2,21 @@ library(shiny)
 
 
 shinyUI(pageWithSidebar(
-  headerPanel("Loading data"),
+  headerPanel("Gene Profile"),
   sidebarPanel(
-    selectInput("dataset", "Choose a dataset:", 
-                choices =gsub("\\.RData","",dir("/home/guegan/Documents/ICM/Dev/shiny/test_vincent/",pattern="*.RData"))),
-    textInput("gene","Gene"),    
-    submitButton("Submit")
-
+    selectInput(inputId="dataset",
+                label="Choose a dataset:", 
+                choices =gsub("_data\\.txt","",dir("/mnt/ihu-3-analyses-users/PROJET/LOBSIGER_GEO_ARRAYEXPRESS/data4shiny",pattern="*_data.txt"))
+                ),
+    
+    uiOutput("pheno"),
+    textInput("gene","Gene"),
+    submitButton(text = "Submit")
+    
   ),
+  
   mainPanel(
+    conditionalPanel(condition="$('html').hasClass('shiny-busy')",img(src="http://i1215.photobucket.com/albums/cc508/nawel12/loading.gif")),
     plotOutput("view")
   )
 ))
